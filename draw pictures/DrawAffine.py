4f07d51163c3a23c2a3c1a61787c0bf0c4d94eb4
Robin Chen <15011074486@163.com>
2018-08-23 20:37:31 +0800

    add drawing files

--- draw pictures/DrawAffine.py
+++ draw pictures/DrawAffine.py
@@ -20,6 +20,7 @@ plt.plot(5, 9, 'ro')
 plt.annotate('$x_1$', xy = (5.2, 8.8))
 plt.plot(6, 11, 'ro')
 plt.annotate('$y=x_2+\\theta (x_1-x_2), \\theta = 2$', xy = (6.2, 10.8))
-plt.xlim((0, 10))
+plt.xlim((0, 10)) # x scale
 plt.ylim((0, 16))
+# or plt.axis([0, 10, 0, 16])
 plt.show()
--- stochastic lot sizing/Cash constrained lot sizing/draw_Gy.py
+++ stochastic lot sizing/Cash constrained lot sizing/draw_Gy.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-@date: Created on Thu Aug 23 17:00:53 2018 
+@date: Created on Thu Aug 23 17:00:53 2018
 
 @author: Zhen Chen
 
 @Python version: 3.6
 
 @descprition: draw G(y) for the last period
-    
+
 """
 
 import matplotlib.pyplot as plt
@@ -19,22 +19,21 @@ iniCash = 110;
 fixOrderCost = 100;
 variCost = 1;
 price = 8;
-holdCost = 1;
+holdCost = 5;
 
 Gy = []
-max_demand = sp.poisson.ppf(0.99, meanDemand)
+max_demand = sp.poisson.ppf(0.99, meanDemand).astype(int)
 for y in range(100):
     singleValue = 0
     for d in range(max_demand):
         revenue = price * min(y, d)
         Iplus = max(y - d, 0)
         ICost = holdCost * Iplus;
-        fixCost = fixOrderCost if y > 0 else 0 
         orderCost = variCost * y
-        cashIncre = revenue - ICost - fixCost - orderCost
-        singleValue += sp.poisson.pmf(d, meanDemand) * cashIncre
-    
+        cashIncre = revenue - ICost - orderCost
+        singleValue -= sp.poisson.pmf(d, meanDemand) * cashIncre
+
     Gy.append(singleValue)
-
-
- 
+    
+plt.ylim(-500, 0)
+plt.plot(Gy)
