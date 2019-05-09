ccaf08aae5875889aa5c5bc2509d07d3a4e7b48c
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2019-05-09 20:39:49 +0800

    update power project

---
+++ Chonqqing Power Grid/forecast_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 19:36:30 2019
+
+@author: Zhen Chen
+
+@email: okchen321@163.com 
+
+Python version： 3.7
+
+Description: forecast the data 
+    
+"""
+
