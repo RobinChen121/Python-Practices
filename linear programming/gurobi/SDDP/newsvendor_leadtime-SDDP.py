9fc03c08683f04f1a155ee008180175e5182bd57
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2023-08-05 15:45:55 +0800

    update

---
+++ linear programming/gurobi/SDDP/newsvendor_leadtime-SDDP.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  5 15:44:23 2023
+
+@author: zhen chen
+@Email: chen.zhen5526@gmail.com
+
+MIT Licence.
+
+Python version: 3.8
+
+
+Description: SDDP to solve newsvendor with lead time
+    
+"""
