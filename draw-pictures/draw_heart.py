65b6eb510ea78709c38ea779cd0b4b781f74d48b
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2023-09-03 22:50:56 +0800

    Create draw_heart.py

---
+++ draw-pictures/draw_heart.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  3 22:23:11 2023
+
+@author: zhenchen
+
+@disp:  
+    
+    
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+x = np.linspace(-2, 2, 1000)
+y1 = [math.sqrt(2*math.sqrt(i**2) - i**2) for i in x]
+y2 = [-2.14*math.sqrt(math.sqrt(2)-math.sqrt(abs(i))) for i in x]
+
+plt.plot(x, y1, 'r')
+plt.plot(x, y2, 'r')
+plt.fill_between(x, y1, y2, color='r')
+
+plt.show()
