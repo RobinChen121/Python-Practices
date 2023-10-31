a3fc88a3ab5644c3e6ac8468b7342860e4d0304a
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2023-10-31 16:17:17 +0800

    Create multi_product_nobound.py

---
+++ linear programming/gurobi/overdraft-SDDP/multi_product_nobound.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 16:51:06 2023
+
+@author: zhen chen
+@Email: chen.zhen5526@gmail.com
+
+MIT Licence.
+
+Python version: 3.11
+
+
+Description: 
+    
+"""
