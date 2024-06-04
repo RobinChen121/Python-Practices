2847a0255438f91c9d366c86f2337371a9ac4e41
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2024-06-04 11:31:39 +0100

    update

--- linear programming/gurobi/write_to_file.py
+++ linear programming/gurobi/write_to_file.py
@@ -10,5 +10,20 @@ Created on Mon Jun  3 16:17:33 2024
     
 """
 
-def write_to_csv(file_address_name, header, content_array):
+import csv
+
+
+def write_to_csv(file_address_name:str, header:list[str], content_array:list, first_write:bool):
+    # check if content_array is 2D list or 1D list
+    twoD = False
+    if isinstance(content_array[0], list):
+        twoD = True
+    with open (file_address_name, 'a', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)   
+        if first_write == True:
+            csvwriter.writerow(header)
+        if twoD:
+            csvwriter.writerows(content_array)
+        else:
+            csvwriter.writerow(content_array)    
     return
