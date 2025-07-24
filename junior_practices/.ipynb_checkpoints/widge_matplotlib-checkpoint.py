38c37d5dd823e62216eb1d4d98612198e7acac0c
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2025-07-24 23:57:28 +0100

    update

---
+++ junior_practices/.ipynb_checkpoints/widge_matplotlib-checkpoint.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import interact, FloatSlider
+
+# 可交互的绘图函数
+def plot_sine(frequency=1.0):
+    x = np.linspace(0, 2*np.pi, 1000)
+    y = np.sin(frequency * x)
+    plt.figure(figsize=(6, 3))
+    plt.plot(x, y)
+    plt.title(f"sin({frequency}x)")
+    plt.grid(True)
+    plt.show()
+
+# 生成滑块
+interact(plot_sine, frequency=FloatSlider(min=0.1, max=10.0, step=0.1, value=1.0));
